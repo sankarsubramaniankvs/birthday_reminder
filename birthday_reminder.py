76c20b8032b361f7d1d9bbcfc23d9039b47039fa
Sankara Subramanian V <48561545+sankarsubramaniankvs@users.noreply.github.com>
2020-02-04 21:24:38 +0530

    Add files via upload

---
+++ birthday_reminder.py
@@ -0,0 +1,26 @@
+import csv
+import time
+import os
+from win10toast import ToastNotifier
+l=[]
+f=1
+with open('birthday_db.csv') as csv_file:
+    csv_reader = csv.reader(csv_file,delimiter =',')
+    date = time.strftime("%d-%m-%y")
+    date=date[:len(date)-2]
+    for i in csv_reader:
+        t=i[4]
+        t = t[:len(t)-2]
+        if t == date:
+            l.append(i) 
+            f=0
+if(f==1):
+    toaster = ToastNotifier()
+    toaster.show_toast("No Birthdays Today!","Birthdays",threaded=True,icon_path='bday.ico',duration=5)
+else:
+    for i in l:
+        toaster = ToastNotifier()
+        toaster.show_toast("Birthday of " + i[1],"Birthdays",threaded=True,icon_path='bday.ico',duration=5)
+
+    
+       
